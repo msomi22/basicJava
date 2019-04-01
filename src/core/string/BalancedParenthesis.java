8622f06c8edb8996225d1ff0633559bb29941a90
msomi22 <mwendapeter72@gmail.com>
2019-04-01 23:32:58 +0300

    is balanced parenthesis check

--- src/core/string/BalancedParenthesis.java
+++ src/core/string/BalancedParenthesis.java
@@ -1,10 +1,59 @@
 package core.string;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class BalancedParenthesis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] testArr = {"()","((()()()(())))","(()()()())",
+				"(({)})","","([{)}]","((())","([{}])(){}[]", "({[]})"};
 
+		
+		
+		Arrays.asList(testArr).stream().forEach(str -> {
+			System.out.print(str); 
+			StringBuilder response = new StringBuilder();
+			Stack<Character> stack = new Stack<Character>(); 
+			
+			if(str.isEmpty())
+				response.append("Balanced");
+				
+			
+			for(int i=0;i<str.length();i++){
+				char cur = str.charAt(i);
+				if(cur == '{' || cur == '(' || cur == '['){
+					stack.push(cur);
+				}
+				
+				if(cur == '}' || cur == ')' || cur == ']'){
+					if(stack.isEmpty())
+						response.setLength(0); 
+						response.append("Not Balanced");
+					char last = stack.peek();
+					if(cur =='}' && last == '{' || cur == ')' && last == '(' || cur == ']' && last == '[')
+						stack.pop();
+					else
+						response.setLength(0); 
+						response.append("Not Balanced");
+						
+				}
+				
+				String result = stack.isEmpty() ? "Balanced" : "Not Balanced";
+				response.setLength(0); 
+				response.append(result);
+				
+			}
+			System.out.println(" " + response); 
+			
+		});
+		
+		
+		
+		
+		
+		
 	}
 
 }
